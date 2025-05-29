7e9007896bcbb269f449709d660ddd5f762f41cb
lolqrdc <lola-rdc@outlook.fr>
2025-05-29 14:05:21 +0200

    la partie Contact terminée.

--- cpp00/ex01/inc/Contact.hpp
+++ cpp00/ex01/inc/Contact.hpp
@@ -6,7 +6,42 @@
 /*   By: lolq <lolq@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/26 09:16:43 by lolq              #+#    #+#             */
-/*   Updated: 2025/05/26 09:16:44 by lolq             ###   ########.fr       */
+/*   Updated: 2025/05/29 14:04:01 by lolq             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#pragma once
+
+#include <iostream>
+#include <string>
+class   Contact 
+{
+    private:
+    std::string  firstName;
+    std::string  lastName;
+    std::string  phoneNum;
+    std::string  nickname;
+    std::string  secret;
+
+    public:
+    // Constructor by default
+    Contact();
+    // Contructor with params
+    Contact(const std::string& firstName, const std::string& lastName, 
+            const std::string &nickname, const std::string& phoneNum, 
+            const std::string& secret);
+    // Setters: modify private data
+    void    setFirstName(const std::string& firstName);
+    void    setLastName(const std::string& lastName);
+    void    setNickname(const std::string& nickname);
+    void    setPhoneNum(const std::string& phoneNum);
+    void    setSecret(const std::string& secret);
+    // Getters: read only access to private data
+    std::string getFirstName() const;
+    std::string getLastName() const;
+    std::string getNickname() const;
+    std::string getPhoneNum() const;
+    std::string getSecret() const;
+    // Method: action
+    void    displayContactInfo() const;
+};
--- cpp00/ex01/src/Contact.cpp
+++ cpp00/ex01/src/Contact.cpp
@@ -6,7 +6,73 @@
 /*   By: lolq <lolq@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/26 09:14:16 by lolq              #+#    #+#             */
-/*   Updated: 2025/05/26 09:14:17 by lolq             ###   ########.fr       */
+/*   Updated: 2025/05/29 11:52:32 by lolq             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "Contact.hpp"
+#include <iostream>
+
+Contact::Contact() {}
+
+Contact::Contact(const std::string& firstName, const std::string& lastName, 
+                 const std::string& nickname, const std::string& phoneNum, 
+                 const std::string& secret)
+    : firstName(firstName), lastName(lastName), phoneNum(phoneNum), nickname(nickname), secret(secret) {}
+
+void    Contact::setFirstName(const std::string& firstName)
+{
+    this->firstName = firstName;
+}
+
+void    Contact::setLastName(const std::string& lastName)
+{
+    this->lastName = lastName;
+}
+
+void    Contact::setNickname(const std::string& nickname)
+{
+    this->nickname = nickname;
+}
+void    Contact::setPhoneNum(const std::string& phoneNum)
+{
+    this->phoneNum = phoneNum;   
+}
+void    Contact::setSecret(const std::string& secret)
+{
+    this->secret = secret;
+}
+
+std::string Contact::getFirstName() const
+{
+    return (firstName);
+}
+
+std::string Contact::getLastName() const
+{
+    return (lastName);
+}
+
+std::string Contact::getNickname() const
+{
+    return (nickname);
+}
+
+std::string Contact::getPhoneNum() const
+{
+    return (phoneNum);
+}
+
+std::string Contact::getSecret() const
+{
+    return (secret);
+}
+
+void    Contact::displayContactInfo() const
+{
+    std::cout << "First name    : " << firstName << std::endl;
+    std::cout << "Last name     : " << lastName << std::endl;
+    std::cout << "Nickname      : " << nickname << std::endl;
+    std::cout << "Phone Number  : " << phoneNum << std::endl;
+    std::cout << "Secret        : " << secret << std::endl;
+}
