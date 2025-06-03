0ad9717f3ca99f21c272f6a46a1310f96dbd588d
lolqrdc <lola-rdc@outlook.fr>
2025-06-03 17:51:25 +0200

    cpp01/ex02 : terminé mais à revoir.

---
+++ cpp01/ex02/main.cpp
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lolq <lolq@student.42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/03 13:36:29 by lolq              #+#    #+#             */
+/*   Updated: 2025/06/03 17:28:53 by lolq             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <string>
+#include <iostream>
+#include <limits>
+
+int main ()
+{
+    std::string brain = "HI THIS IS BRAIN";
+    std::string *stringPTR = &brain;
+    std::string &stringREF = brain;
+    
+    std::cout << "Adresse brain         : " << &brain << std::endl;
+    std::cout << "Adresse via stringPTR : " << stringPTR << std::endl;
+    std::cout << "Adresse via stringREF : " << &stringREF << std::endl;
+
+    std::cout << "Valeur de brain       : " << brain << std::endl;
+    std::cout << "Valeur via stringPTR  : " << *stringPTR << std::endl;
+    std::cout << "Valeur via stringREF  : " << stringREF << std::endl;
+    return (0);
+}
