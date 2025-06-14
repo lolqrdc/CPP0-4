e3ba651fe8fc14b2cc9cdded0b10ed7129a96255
lolqrdc <lola-rdc@outlook.fr>
2025-06-14 16:25:20 +0200

    modif ex02 cpp01

--- cpp01/ex02/main.cpp
+++ cpp01/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: lolq <lolq@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/03 13:36:29 by lolq              #+#    #+#             */
-/*   Updated: 2025/06/03 17:28:53 by lolq             ###   ########.fr       */
+/*   Updated: 2025/06/14 16:22:08 by lolq             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@ int main ()
     std::string &stringREF = brain;
     
     std::cout << "Adresse brain         : " << &brain << std::endl;
-    std::cout << "Adresse via stringPTR : " << stringPTR << std::endl;
+    std::cout << "Adresse via stringPTR : " << &stringPTR << std::endl;
     std::cout << "Adresse via stringREF : " << &stringREF << std::endl;
 
     std::cout << "Valeur de brain       : " << brain << std::endl;
