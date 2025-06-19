f1d6460cf032118c38b2870f38db42220a4447b0
lolqrdc <lola-rdc@outlook.fr>
2025-06-19 09:25:40 +0200

    ex01 : changement dans le main.

--- cpp03/ex01/src/main.cpp
+++ cpp03/ex01/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: lolq <lolq@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/17 10:24:59 by lolq              #+#    #+#             */
-/*   Updated: 2025/06/18 17:19:32 by lolq             ###   ########.fr       */
+/*   Updated: 2025/06/19 09:25:02 by lolq             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,61 +15,40 @@
 int main()
 {
     std::cout << "=== Test : constructeurs ===" << std::endl;
-    
-    // constructeur par defaut
     ScavTrap defaultScav;
-    
-    // constructeur avec nom
     ScavTrap namedScav("Guardian");
-    
-    // constructeur de copie
     ScavTrap copyScav(namedScav);
     
     std::cout << "\n=== Test : fonctions de base ===" << std::endl;
-    
-    // attack
     namedScav.attack("Enemy1");
     namedScav.attack("Enemy2");
-    
-    // takeDamage
     namedScav.takeDamage(30);
     namedScav.takeDamage(50);
-    
-    // beRepaired
     namedScav.beRepaired(20);
     
-    std::cout << "\n=== Test : fonction speciale (guardGate) ===" << std::endl;
-    
-    // guardGate en vie
+    std::cout << "\n=== Test : GuardGate ===" << std::endl;
     namedScav.guardGate();
     
     std::cout << "\n=== Test : un ScavTrap mort ===" << std::endl;
-    
-    // nouveau ScavTrap et le tuer
     ScavTrap deadScav("DeadBot");
     deadScav.takeDamage(100);
-    
     // Tester les fonctions sur un ScavTrap mort
     deadScav.attack("Target");
     deadScav.beRepaired(10);
     deadScav.guardGate();
     
     std::cout << "\n=== Test : l'op d'assignation ===" << std::endl;
-    
     ScavTrap assignedScav;
     assignedScav = namedScav;
     assignedScav.guardGate();
     
     std::cout << "\n=== Test : d'out of energy ===" << std::endl;
-    
     ScavTrap energyTest("EnergyBot");
-    
     // Run out of l'energie
-    for (int i = 0; i < 51; i++) {
+    for (int i = 0; i < 51; i++) 
+    {
         energyTest.attack("Target");
     }
-    
     std::cout << "\n=== Fin des tests - Destruction des objets ===" << std::endl;
-    
-    return 0;
+    return (0);
 }
